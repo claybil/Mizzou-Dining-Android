15ee6f539bc7ab66c6bb7862414b4e0d70f4f62e
Clay Billadeau <123415626+claybil@users.noreply.github.com>
2024-01-24 11:20:28 -0600

    Add files via upload

--- app/src/main/java/com/example/myapplication/ui/locations/LocationsFragment.java
+++ app/src/main/java/com/example/myapplication/ui/locations/LocationsFragment.java
@@ -1,1 +1,37 @@
-h
+package com.example.myapplication.ui.locations;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.myapplication.databinding.FragmentLocationsBinding;
+
+public class LocationsFragment extends Fragment {
+
+    private FragmentLocationsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        LocationsViewModel locationsViewModel =
+                new ViewModelProvider(this).get(LocationsViewModel.class);
+
+        binding = FragmentLocationsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+//        final TextView textView = binding.textDashboard;
+//        locationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
